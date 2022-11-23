b1fc57fa72e76033a1908320f1042d942a9fae52
Zziping <Z_ziping@163.com>
2022-11-23 22:14:19 +0800

    2022/11/23/latest1

---
+++ WidgetTest/app/src/main/java/com/android/widgettest/UIBestActivity.kt
@@ -0,0 +1,11 @@
+package com.android.widgettest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UIBestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_uibest)
+    }
+}
