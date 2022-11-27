ad87581b0d4befeedf2aa3cca0d136ab3ed64fad
Zziping <Z_ziping@163.com>
2022-11-28 01:00:52 +0800

    2022/11/28/1

---
+++ BroadcastReceiverApplication/app/src/main/java/com/android/broadcastreceiverapplication/MyBroadcastReceiver.kt
@@ -0,0 +1,13 @@
+package com.android.broadcastreceiverapplication
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class MyBroadcastReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        TODO("MyBroadcastReceiver.onReceive() is not implemented")
+    }
+}
